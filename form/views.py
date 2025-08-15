b8f218b2ca2f0d09a75553153bb53b5e10af52d0
Naum <nvlavceski542@gmail.com>
2025-08-15 14:14:54 +0200

    contact upgrade

--- form/views.py
+++ form/views.py
@@ -1,37 +1,41 @@
-from tkinter.font import names
 
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.shortcuts import render, redirect
-
 from form.forms import ContactForm
 
+import time
 
 # Create your views here.
 
 def contact(request):
     if request.method == "POST":
+        last_sent = request.session.get("last_contact_time", 0)
+        now = time.time()
+        if now - last_sent < 30:
+            return redirect("contact")
+
         form = ContactForm(request.POST)
         if form.is_valid():
             name = form.cleaned_data['name']
             email = form.cleaned_data['email']
             message = form.cleaned_data['message']
             full_message = f"Име: {name}\nEmail: {email}\nMessage: {message}"
-            print(full_message)
-            send_mail(
+            mail = EmailMessage(
                 subject="Контакт Порака",
-                message=full_message,
-                from_email=email,
-                recipient_list=["nvlavceski542@gmail.com"],
-                fail_silently=False,
-
+                body=full_message,
+                from_email="nvlavceski542@gmail.com",
+                to=["nvlavceski542@gmail.com"],
+                reply_to=[email]
             )
+            mail.send(fail_silently=False)
+
+            # Запиши кога последно прати порака
+            request.session["last_contact_time"] = now
+
             return redirect("index")
     else:
         form = ContactForm()
-    return render(request, "contact.html", {"form":form})
+    return render(request, "contact.html", {"form": form})
 
 def index(request):
     return render(request, "index.html")
-
-def success(request):
-    return render(request,"success.html")
